0a07892c11406c95f2b603dd553e83c622c79f54
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-03-16 10:18:03 +0100

    De automatische commits toegevoegd

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 # !/user/bin/env bash
 
 echo "Hello bash world!"
+
+git status
+git add .
+git status
+echo "Geef het bericht op voor uw commit."
+read message
+echo "$message"
+git commit -m "$message"
